b21cb7a8d8551846f4166b2ba2a31fb3d31b0762
Dusan-Milekic <dusanmilekic0511@gmail.com>
2025-10-02 19:00:14 +0200

    configuring tailwind

--- wow-app/tailwind.config.js
+++ wow-app/tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     screens: {
       'sm': '640px',
-      'md': '960px',
+      'md': '960px',  // Custom md breakpoint at 960px
       'lg': '1024px',
       'xl': '1280px',
       '2xl': '1536px',
