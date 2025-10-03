3337b6d249cfffb47fb612e5edcc643547be0e2c
Dusan-Milekic <dusanmilekic0511@gmail.com>
2025-10-03 16:27:58 +0200

    added hero section

---
+++ wow-app/src/components/Hero.tsx
@@ -0,0 +1,13 @@
+export default function Hero() {
+    return (
+        <>
+            <video src="https://res.cloudinary.com/dot9iikam/video/upload/v1759500850/video_wow_1_cruufv.mp4" autoPlay loop muted className="hidden leading-none absolute w-full top-0 -z-10 md:block"></video>
+
+            <img
+                src="MidnightMobilesm.png"
+                alt="Midnight"
+                className="w-full h-full object-cover object-top absolute top-0 -z-10 md:hidden"
+            />
+        </>
+    )
+}
