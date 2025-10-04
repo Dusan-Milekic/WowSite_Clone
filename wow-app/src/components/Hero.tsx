a529d34879dd214ac244d344762b721ec14cde09
Dusan-Milekic <dusanmilekic0511@gmail.com>
2025-10-04 07:21:09 +0200

    Update Hero.tsx

--- wow-app/src/components/Hero.tsx
+++ wow-app/src/components/Hero.tsx
@@ -1,7 +1,13 @@
 export default function Hero() {
     return (
         <>
-            <video src="https://res.cloudinary.com/dot9iikam/video/upload/v1759500850/video_wow_1_cruufv.mp4" autoPlay loop muted className="hidden leading-none absolute w-full top-0 -z-10 md:block"></video>
+            <video
+                src="https://res.cloudinary.com/dot9iikam/video/upload/v1759500850/video_wow_1_cruufv.mp4"
+                autoPlay
+                loop
+                muted
+                className="hidden leading-none absolute w-full h-[800px] top-0 -z-10 md:block object-cover"
+            ></video>
 
             <div className="holder h-[800px] relative">
                 <img
@@ -9,11 +15,16 @@ export default function Hero() {
                     alt="Midnight"
                     className="w-full h-full object-cover object-top absolute top-0 -z-10 md:hidden"
                 />
-                <img part="image" className="absolute top-96 translate-y-10 w-[420px] left-1/2 -translate-x-1/2" src="https://blz-contentstack-images.akamaized.net/v3/assets/blt3452e3b114fab0cd/blte82ef8135378a09b/5d4883cabf384b03afd01553/Logo-wow.png" alt="World of Warcraft: Midnight Logo" loading="lazy"></img>
+                <img part="image" className="absolute top-96 translate-y-10 w-[420px] left-1/2 -translate-x-1/2 md:top-8 md:w-[480px] md:translate-x-0 md:ml-60 md:left-0" src="https://blz-contentstack-images.akamaized.net/v3/assets/blt3452e3b114fab0cd/blte82ef8135378a09b/5d4883cabf384b03afd01553/Logo-wow.png" alt="World of Warcraft: Midnight Logo" loading="lazy"></img>
             </div>
-            <div className="text relative -top-48 w-[453px] mx-auto space-y-3">
-                <h1 className="text-4xl text-center font-bold font-mavenPro uppercase">World of Warcraft: Midnight Revealed!</h1>
-                <p className="text-center font-mavenPro">Return to the elven kingdom of Quel'Thalas as Xal’atath’s invasion of Azeroth begins, and the Voidstorm threatens to cover the world in darkness.</p>
+            <div className="text relative -top-48 w-[453px] mx-auto space-y-3 md:ml-32 md:-translate-y-80 md:w-[720px] 2xl:w-[773px] 2xl:ml-24">
+                <h1 className="text-4xl text-center font-bold font-mavenPro uppercase 2xl:text-5xl">World of Warcraft: Midnight Revealed!</h1>
+                <p className="text-center font-mavenPro 2xl:text-xl opacity-80">Return to the elven kingdom of Quel'Thalas as Xal’atath’s invasion of Azeroth begins, and the Voidstorm threatens to cover the world in darkness.</p>
+                <div className="flex gap-5 justify-center pt-10
+                ">
+                    <button className="w-64 h-[74px] bg-[#aa2409cc] border border-[#FF2B00] text-[#F8B700] text-2xl">PRE-PURCHASE</button>
+                    <button className="w-60 h-[74px] bg-[#0000004d] border border-[#F8B700] text-[#F8B700] text-2xl">LEARN MORE</button>
+                </div>
             </div>
 
 
